02c236990cbd05e2ee971bdd31ec39f5a8c212e4
Alex Patterson <alex@codingcat.dev>
2021-12-19 13:55:56 -0500

    initial working example

---
+++ src/lib/BuilderPage.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let entry;
+</script>
+
+<builder-component name="page" {entry} api-key="7586388966a8498d98024cc1a85db023">
+	<slot>Loading...</slot>
+</builder-component>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	let BuilderPage;
+
+	onMount(async () => {
+		BuilderPage = (await import('$lib/BuilderPage.svelte')).default;
+	});
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<script async src="https://cdn.builder.io/js/webcomponents"></script>
+</svelte:head>
+
+<svelte:component this={BuilderPage} entry={'b50a2489d5db49cd856f2b36f0eb61c8'}>
+	<p>Loading...</p>
+</svelte:component>
