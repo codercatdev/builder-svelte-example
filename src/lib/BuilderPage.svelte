13509767d26881d9831e8fe10f77773361868899
Alex Patterson <alex@codingcat.dev>
2021-12-19 14:11:39 -0500

    working hardcoded pageId

--- src/lib/BuilderPage.svelte
+++ src/lib/BuilderPage.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <builder-component name="page" {entry} api-key="7586388966a8498d98024cc1a85db023">
-	<slot>Loading...</slot>
+	<!-- TODO: Slot causes failure -->
+	<!-- <slot>Loading...</slot> -->
 </builder-component>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,4 @@
 	<script async src="https://cdn.builder.io/js/webcomponents"></script>
 </svelte:head>
 
-<svelte:component this={BuilderPage} entry={'b50a2489d5db49cd856f2b36f0eb61c8'}>
-	<p>Loading...</p>
-</svelte:component>
+<svelte:component this={BuilderPage} entry={'b50a2489d5db49cd856f2b36f0eb61c8'} />
